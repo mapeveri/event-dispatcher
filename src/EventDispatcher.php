0705c1f6b8b3390430fd02c93c670a400027eaff
Martin Peveri <martinpeveri@gmail.com>
2022-05-04 21:07:24 +0200

    First commit

---
+++ src/EventDispatcher.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace EventDispatcher;
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use Psr\EventDispatcher\StoppableEventInterface;
+
+class EventDispatcher implements EventDispatcherInterface
+{
+    public function __construct(private ListenerProviderInterface $listenerProvider)
+    {
+    }
+
+    public function dispatch(object $event): object
+    {
+        if ($event instanceof StoppableEventInterface && $event->isPropagationStopped()) {
+            return $event;
+        }
+
+        foreach ($this->listenerProvider->getListenersForEvent($event) as $listener) {
+            $listener($event);
+        }
+
+        return $event;
+    }
+}
